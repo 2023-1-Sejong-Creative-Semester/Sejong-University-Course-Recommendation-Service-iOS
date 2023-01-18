512cd2d04d26be3a6ea8aaba9bf6565eafcaa45e
ShapeKim98 <talkers.potters-0p@icloud.com>
2023-01-18 22:26:08 +0900

    initial Project

---
+++ SJUCourseRecommendationService/SJUCourseRecommendationService/SJUCourseRecommendationServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SJUCourseRecommendationServiceApp.swift
+//  SJUCourseRecommendationService
+//
+//  Created by 김도형 on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct SJUCourseRecommendationServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
